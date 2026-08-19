eb98faa15ae88fb3631f5bc17934fbfab3203a3d
Vishnu18511 <gojo74352@gmail.com>
2026-08-19 16:54:10 +0530

    Add files via upload

---
+++ d3/p3.java
@@ -0,0 +1,15 @@
+package d3;
+
+	class p3 {
+		public static void main(String[] args) {
+			System.out.println("anand");
+			System.out.println("anand3");
+			int a = 10 / 0;
+			try {
+				System.out.println("kishore");
+			} finally {
+				System.out.println("finally block");
+			}
+			System.out.println("rest of the code");
+		}
+	}
---
+++ d3/p5.java
@@ -0,0 +1,29 @@
+package d3;
+
+//public class p5 {
+	class BankService {
+		static void withdrawMoney(int balance, int amount) throws Exception {
+			checkBalance(balance, amount);
+			System.out.println("Withdrawal successful");
+		}
+		static void checkBalance(int balance, int amount) throws Exception {
+
+			if (amount > balance) {
+				throw new Exception("Insufficient Balance");
+			}
+			System.out.println("Balance is sufficient");
+		}
+	}
+	public class p5 {
+
+		public static void main(String[] args) {
+
+			try {
+				BankService.withdrawMoney(2000, 5000);
+			} catch (Exception e) {
+				System.out.println("Transaction Failed: " + e.getMessage());
+			}
+
+		}
+	}
+
