3284dbbc973a837395b49351cf4b699983e5d6f1
Vishnu18511 <gojo74352@gmail.com>
2026-09-01 16:30:45 +0530

    Add files via upload

---
+++ src/d10/p7.java
@@ -0,0 +1,21 @@
+package d10;
+import java.util.PriorityQueue;
+
+import java.util.Collections;
+
+public class p7 {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        maxHeap.add(30);
+        maxHeap.add(10);
+        maxHeap.add(20);
+        maxHeap.add(5);
+
+        System.out.println("Max Heap: " + maxHeap);
+
+        System.out.println("Largest Element: " + maxHeap.peek());
+    }
+}
