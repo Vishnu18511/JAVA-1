e5dd747dd7317c6dba34c4b55485f5c764c820fb
Vishnu18511 <gojo74352@gmail.com>
2026-09-03 16:47:47 +0530

    Add files via upload

---
+++ src/d12/p9.java
@@ -0,0 +1,25 @@
+package d12;
+
+public class p9 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 0, 1, 0, 3, 12 };
+
+		int[] result = new int[arr.length];
+
+		int index = 0;
+
+		for (int num : arr) {
+
+			if (num != 0) {
+				result[index] = num;
+				index++;
+			}
+		}
+
+		for (int num : result) {
+			System.out.print(num + " ");
+		}
+	}
+}
